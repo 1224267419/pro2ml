1f2d5e3b42998cacae3d3d5687c63d65c7875154
tangxia <1224267419@qq.com>
2025-02-22 22:33:33 +0800

    2-22-XGBoost-otto-demo

--- 18_xgboost_otto_demo.py
+++ 18_xgboost_otto_demo.py
@@ -8,7 +8,8 @@ from sklearn.model_selection import StratifiedShuffleSplit
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-
+from xgboost import XGBClassifier
+from sklearn.metrics import log_loss
 def demo0():#分层随即划分的用法
     X = np.array([[1, 2], [3, 4], [1, 2], [3, 4], [1, 2]])
     Y = np.array([0, 0, 0, 1, 1])
@@ -56,12 +57,14 @@ def demo1():#业务代码
 
     sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=0)
     #分层采样,nsplist即仅使用分层采样而不重复采样
+    y_val=y_test
+    x_val=x_test
     for train_index, test_index in sss.split(x_train, y_train):
         x_train=X_resampled.values[train_index]
         x_val=X_resampled.values[test_index]
 
         y_train=y_resampled[train_index]
-        y_test=y_resampled[test_index]
+        y_val=y_resampled[test_index]
 
     # sns.countplot(y_resampled)
     # plt.show()
@@ -82,10 +85,55 @@ def demo1():#业务代码
     # 原数据特征数目为: 93
     # 表达90 % 的信息只需要: 65个特征
 
+    #模型训练
+    #基本模型
+    xgb=XGBClassifier()
+    xgb.fit(x_train_pca, y_train)
+
+    #改变预测值输出模式,
+    y_pre_proba=xgb.predict_proba(x_val_pca)
+    # print(y_pre_proba)
 
 
 
+    loss1=log_loss(y_val,y_pre_proba,normalize=True)
+    print( "默认参数下的xgboost的loss值为" ,  loss1)
+    #默认loss=0.7457300787373345
+    # #参数查询
+    # print(xgb.get_params)
 
+    #模型调参
+    scores_ne = []
+    n_estimators = [100, 200, 300, 400, 500, 550, 600, 700]
+    for nes in n_estimators:
+        # print("n_estimators:", nes)
+        xgb = XGBClassifier(max_depth=3,
+                            learning_rate=0.1,
+                            n_estimators=nes,
+                            objective="multi:softprob",
+                            n_jobs=-1,
+                            nthread=4,
+                            min_child_weight=1,
+                            subsample=1,
+                            colsample_bytree=1,
+                            seed=42)
+
+        xgb.fit(x_train_pca, y_train)
+        y_pre = xgb.predict_proba(x_val_pca)
+        score = log_loss(y_val, y_pre)
+        scores_ne.append(score)
+
+    # 图形化展示对应的logloss值
+    plt.plot(n_estimators, scores_ne, "o-")
+
+    plt.xlabel("n_estimators")
+    plt.ylabel("log_loss")
+    plt.show()
+
+    print("最优的n_estimators值是:{}".format(n_estimators[np.argmin(scores_ne)]))
+    print(np.argmin(scores_ne))
+    #最优的n_estimators值是:400 ,此时的logloss值是:0.6943845593586594
+    
 if __name__ == '__main__':
     # demo0()
     demo1()
